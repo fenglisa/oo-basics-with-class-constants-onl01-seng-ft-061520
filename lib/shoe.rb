35f2d39ebc8755acb574fca88a3a73c25b1e37d0
Lisa Feng <lisafeng10@gmail.com>
2020-06-24 04:21:57 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,15 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
+  
+  BRANDS = []
 
   def initialize(brand)
     @brand = brand
+    BRANDS << brand
+    if BRANDS != BRANDS.uniq
+      BRANDS.pop
+    end
   end
 
   def cobble
